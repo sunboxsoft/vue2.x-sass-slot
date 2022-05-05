da949394805d0f581ca28c072780962aa2e2f420
“sunboxsoft” <sunboxsoft@126.com>
2022-05-05 21:36:47 +0800

    路由导航式编程

--- src/components/Father.vue
+++ src/components/Father.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <router-link to="/father">Father</router-link>
-    <router-link :to="{name:'ChildA',params:{username:'jiaguoqiang'}}">ChildA</router-link>
+    <router-link :to="{ name: 'ChildA', params: { username: 'jiaguoqiang' } }"
+      >ChildA</router-link
+    >
     <router-link to="/father/childb">ChildB</router-link>
-    <router-link to="/father/childc/123/news_id123">
-    ChildC</router-link>
-    <transition name="fade" mode="in-out">
-    <router-view /></transition>
+    <router-link to="/father/childc/123/news_id123"> ChildC</router-link>
+    <button @click="$router.go(1)">前进</button>
+    <button  @click="$router.go(-1)">后退</button>
+    <button  @click="$router.push('/')">编程式导航跳转</button>
+    <transition name="fade" mode="in-out"> <router-view /></transition>
   </div>
 </template>
 
@@ -25,19 +28,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
- .fade-enter-active, .fade-leave-active {
-        transition: all 0.3s ease;
-    }
-    .fade-enter{
-        transform: translateX(-200%);
-    }
-    .fade-enter-to{
-        transform: translateX(-100%);
-    }
-    .fade-leave{
-        transform: translateX(0);
-    }
-    .fade-leave-to{
-        transform: translateX(100%);
-    }
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-enter {
+  transform: translateX(-200%);
+}
+.fade-enter-to {
+  transform: translateX(-100%);
+}
+.fade-leave {
+  transform: translateX(0);
+}
+.fade-leave-to {
+  transform: translateX(100%);
+}
 </style>
